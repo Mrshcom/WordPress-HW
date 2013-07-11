a0ea3bd13141822d9dd54940eccf6aa35a0a8ec8
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-07-11 16:10:06 +0430

    add content

---
+++ content.php
@@ -0,0 +1,96 @@
+	<div class="container-content">
+		<div class="graybar"></div>
+		<div class="content">
+			<div class="outer-content">
+				<div class="inner-content">
+					<section class="slider"></section>
+					<section class="subslider">
+						<h2> به ما اجازه دهید تا بهترین پوشش های بیمه زندگی را برایتان پیدا کنیم</h2>
+						<p>
+							ما به عنوان متخصص بیمه های زندگی به شما اطمینان می دهیم که با مقایسه قیمت و شرایط 20 شرکت بیمه موجود در کشور.
+							<br />
+							ما به عنوان متخصص بیمه های زندگی به شما اطمینان می دهیم که با مقایسه قیمت و شرایط 20 شرکت بیمه موجود در کشور،مناسب ترین پوشش را برایتان پیدا می کنیم.
+						</p>
+					</section>
+					<section class="rapid-access cover">
+						<section class="links">
+							<h3>
+								دسترسی سریع
+								<span class="greenbar"></span>
+							</h3>
+							<nav>
+								<?php
+									wp_nav_menu(array(
+										'theme_location'  => 'RapidAccess',
+										'container'       => false, 
+										'container_class' => '', 
+										'menu_class'      => '', 
+										'before'          => '',
+										'after'           => '',
+										'link_before'     => '',
+										'link_after'      => '',
+										'items_wrap'      => '<ul class="%2$s">%3$s</ul>'
+									));
+								?>
+							</nav>
+						</section>
+						<section class="Insurance">
+							<div class="rows">
+								<article class="right">
+									<a href="#">
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه عمر</p>
+								</article>
+								<article class="right">
+									<a href="#">
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه تمام عمر</p>
+								</article>
+								<article class="right">
+									<a href="#">
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه مستمری و بازنشستگی</p>
+								</article>
+								<article class="mra last">
+									<a href="#">
+										<span class="commentimg">یک ساله 280.000</span>
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه عمر</p>
+								</article>
+							</div>
+							<div class="rows">
+								<article class="right">
+									<a href="#">
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه حوادث</p>
+								</article>
+								<article class="right">
+									<a href="#">
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه زنان خاندار</p>
+								</article>
+								<article class="right">
+									<a href="#">
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه درمان</p>
+								</article>
+								<article class="mra last">
+									<a href="#">
+										<img src="<?php bloginfo('template_url'); ?>/images/insurance_list.jpg" alt="Insurance">
+									</a>
+									<p>بیمه عمر</p>
+								</article>
+							</div>
+						</section>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
