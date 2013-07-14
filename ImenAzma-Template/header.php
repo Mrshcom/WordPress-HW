413a85ec3e70257c0526203fb39c5a981e0dda0b
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-07-15 00:52:07 +0430

    add header.php

---
+++ ImenAzma-Template/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="fa-IR" dir="rtl" >
+<head>
+  <meta charset="UTF-8" />
+  <title><?php bloginfo('name'); wp_title(); ?></title>
+  <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style/1styles.css" />
+  <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" />
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style/nivo-slider.css" type="text/css" media="screen" />
+  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/html5shiv.js" ></script>
+  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/jquery.js" ></script>
+  <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/script.js" ></script>
+  <!-- <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> -->
+</head>
+<body>
