f294d2b0487ad04df7a63aee95596411674231d4
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-07-15 00:47:55 +0430

    add script

---
+++ ImenAzma-Template/script/script.js
@@ -0,0 +1,103 @@
+$(function(){
+
+	var newsTrain = $('div.projects-list div.newsMainCase div.newsTrain'),
+		lists = $('div.projects-list div.newsMainCase div.newsTrain ul li'),
+		newsMainCase = $('div.projects-list div.newsMainCase'),
+		iheight = lists.height(),
+		isize = lists.size(),
+		num2case = 2, // 7 /// num of li(es) in newsMainCase that are visible
+		itop = 0;
+
+	(slideNews = function(){
+		if (itop >= (iheight*isize - (num2case-1)*iheight) ) itop = 0;
+		newsTrain.animate({top:-itop},800);
+		itop += iheight+4; // because li doesn't height for matching with Sliding
+	})();	
+
+	var intervalName = false;
+	(startSliding = function(){
+		if(intervalName) return;
+		intervalName = setInterval(slideNews,1000);
+	})();
+
+	stopSliding = function(){
+		clearInterval(intervalName);
+		intervalName = false;
+	}
+	
+	newsMainCase.mouseover(stopSliding).mouseout(startSliding); 
+
+	//animate progress bars
+	var hiders=$('div.content div.projects-status ul div.hider'),
+		percentages=$('div.content div.projects-status ul span.percentage');
+	for (var i = 0; i <= percentages.length - 1; i++) {
+		str=percentages.eq(i).text();
+		str.replace("%","");
+		percent=parseInt(str);
+		percentages.eq(i).animate({top:85-percent+'px'},900);
+		hiders.eq(i).animate({height:100-percent+'px'},900);
+	};
+
+
+	// project slider
+	var slideshow = $('div.project-slider'),
+		train = $('div.project-slider div.case-train div.train'),
+		lists = $('div.project-slider ul.nav-bar > li'),
+		currentSlide = 0;
+
+	(show_pic = function(n){
+		if(n>lists.length-1) n=0;
+		if(n<0) n=lists.length-1;
+		train.css({
+			left : (-625*n) + 'px'
+		});
+		lists.eq(currentSlide).removeClass('active');
+		lists.eq(n).addClass('active');
+		currentSlide = n;
+	})(0);
+
+	next_slide = function(){
+		show_pic(currentSlide + 1);
+	}
+	
+	back_slide = function(){
+		show_pic(currentSlide - 1);
+	}
+	
+	var intervalName = false;
+	(startSliding = function(){
+		if(intervalName) return;
+		intervalName = setInterval(next_slide,4000);
+	})();
+	
+	stopSliding = function(){
+		if(!intervalName) return;
+		clearInterval(intervalName);
+		intervalName = false;
+	}
+	
+	for(var i=0; i<lists.length; i++){
+		(function(j){
+			lists.eq(j).click(function(){
+				show_pic(j);
+			})
+		})(i);
+	}	
+	
+	slideshow.mouseover(stopSliding);
+	slideshow.mouseout(startSliding);
+	// end project slider
+
+	$('header section.header div.login a').click(function(){
+		$('header section.header div.loginpanel').animate({
+				top:'5px'
+			},300);
+	});
+
+	$('header section.header div.loginpanel a.close').click(function(){
+		$('header section.header div.loginpanel').animate({
+				top:'-500px'
+			},200);
+	});
+
+});
