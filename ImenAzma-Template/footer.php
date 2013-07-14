fc031843a74e37fcbf9114476cb2186d035cbb89
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-07-15 00:52:17 +0430

    add footer.php

---
+++ ImenAzma-Template/footer.php
@@ -0,0 +1,41 @@
+
+  <div class="fcontainers">
+    <footer class="w24 mlra cover overhide">
+      <div class="w10 mh4 pb1 pt1 left address">
+        <p>
+          <span>آدرس شرکت: </span>مشهد - بلوار سجاد - خیابان خیام - خیام 23 -پلاک 600<br/>
+          <span>تلفن تماس: </span>7658662-0511
+          &ensp;&ensp;&ensp;&ensp;
+          <span>دورنگار: </span>7658662-0511<br/>
+          <span>ایمیل: </span><a href="mailto:info@ImenAzma.org">info@ImenAzma.org</a><br/>
+        </p> 
+      </div>
+      <div class="w4 mh4 left counter last">
+        <!-- Counter /ARBITRARY -->
+      </div>
+      <div class="w9 mh4 pt1 mla cover last">
+        <section class="fmenu">
+          <ul>
+            <li><a href="">صفحه اصلی</a></li>
+            <li><a href="">تماس با ما</a></li>
+            <li><a href="">گالری تصاویر</a></li>
+          </ul>
+        </section>
+        <section class="right copyright">
+          <p>
+             طراحی و بهینه سازی سایت توسط
+             &ensp;<a href="http://www.Mampel.ir" title="Mampel Design">Mampel Design</a>
+          </p>
+        </section>
+      </div>
+      <div class="lightbox"></div>
+    </footer>
+  </div>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/script/jquery.nivo.slider.js"></script>
+    <script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>
+</body>
+</html>
