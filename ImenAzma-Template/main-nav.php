004890b4663e13cc17603e045f2054830ebd1d1b
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-07-15 00:52:34 +0430

    add main nav

---
+++ ImenAzma-Template/main-nav.php
@@ -0,0 +1,54 @@
+<?php
+  wp_nav_menu(array(
+    'theme_location'  => 'MainNav',
+    'container'       => false, 
+    'container_class' => '', 
+    'menu_class'      => '', 
+    'before'          => '',
+    'after'           => '',
+    'link_before'     => '',
+    'link_after'      => '',
+    'items_wrap'      => '<menu class="%2$s">%3$s</menu>'
+  ));
+?>
+<!-- <menu>
+  <li>
+    <a href="#">نخست</a>
+  </li>
+  <li>
+    <a href="#">
+      معرفی شرکت‌های زیرمجموعه<span></span>
+    </a>
+    <menu>
+      <li><a href="#">شرکت الوان پارس</a></li>
+      <li><a href="#">صنایع اراک</a></li>
+      <li><a href="#">آژند برج</a></li>
+      <li><a href="#">آژند مفتول</a></li>
+    </menu>
+  </li>
+  <li>
+    <a href="#">
+      معرفی واحدها<span></span>
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      سیستم‌های مدیریتی<span></span>
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      گالری تصاویر<span></span>
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      درباره ما<span></span>
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      تماس با ما<span></span>
+    </a>
+  </li>
+</menu> -->
